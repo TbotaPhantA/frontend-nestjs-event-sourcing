6478edddd8d60cae829aaf3d31dddfde20e4c461
TbotaPhantA <botakiller@mail.ru>
2024-11-27 21:22:10 +0000

    data data changes

--- src/app/warehouse/page.tsx
+++ src/app/warehouse/page.tsx
@@ -3,6 +3,9 @@ import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import TableStockItems from "@/components/Tables/TableStockItems";
+import { StockItem } from "@/types/stockItem";
+import { TemperatureModeEnum } from "@/types/enums/temperatureMode.enum";
+import { InventoryAdjustmentEnum } from "@/types/enums/inventoryAdjustment.enum";
 
 export const metadata: Metadata = {
   title: "Next.js Tables | TailAdmin - Next.js Dashboard Template",
@@ -10,12 +13,77 @@ export const metadata: Metadata = {
     "This is Next.js Tables page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
 };
 
-const TablesPage = () => {
+const fetchStockItems = async () => {
+  const stockItemData: StockItem[] = [
+    {
+      id: "4c57581f-3051-4b51-b5bb-2fc79c8bb2a1",
+      itemName: "Aluminum",
+      description: "Rolls of aluminum",
+      isFlammable: false,
+      isFragile: false,
+      temperatureMode: TemperatureModeEnum.WITHOUT,
+      weightGrams: 100000,
+      inventoryAdjustment: InventoryAdjustmentEnum.NONE,
+      locationId: "215",
+      createdAt: new Date("2024-11-26T21:32:44.198Z"),
+      updatedAt: new Date("2024-11-26T21:32:44.198Z"),
+      removedAt: null,
+    },
+    {
+      id: "4c57581f-3051-4b51-b5bb-2fc79c1bb2a1",
+      itemName: "Cotton",
+      description: "Bags of cotton",
+      isFlammable: true,
+      isFragile: false,
+      temperatureMode: TemperatureModeEnum.WITHOUT,
+      weightGrams: 1000,
+      inventoryAdjustment: InventoryAdjustmentEnum.SURPLUS,
+      locationId: "215",
+      createdAt: new Date("2024-11-26T21:32:44.198Z"),
+      updatedAt: new Date("2024-11-26T21:32:44.198Z"),
+      removedAt: null,
+    },
+    {
+      id: "4c57581f-3051-4b51-b5bb-2fc79c7bb2a1",
+      itemName: "Meat",
+      description: "Bags with chicken meat",
+      isFlammable: false,
+      isFragile: false,
+      temperatureMode: TemperatureModeEnum.MINUS_15,
+      weightGrams: 10000,
+      inventoryAdjustment: InventoryAdjustmentEnum.NONE,
+      locationId: "215",
+      createdAt: new Date("2024-11-26T21:32:44.198Z"),
+      updatedAt: new Date("2024-11-26T21:32:44.198Z"),
+      removedAt: null,
+    },
+    {
+      id: "4c57581f-3051-4b51-b5bb-2fc79c7bb2a1",
+      itemName: "Electronics",
+      description: "PC components",
+      isFlammable: false,
+      isFragile: true,
+      temperatureMode: TemperatureModeEnum.WITHOUT,
+      weightGrams: 15000,
+      inventoryAdjustment: InventoryAdjustmentEnum.NONE,
+      locationId: "215",
+      createdAt: new Date("2024-11-26T21:32:44.198Z"),
+      updatedAt: new Date("2024-11-26T21:32:44.198Z"),
+      removedAt: null,
+    },
+  ];
+
+  return { stockItems: stockItemData };
+};
+
+const TablesPage = async () => {
+  const { stockItems } = await fetchStockItems();
+
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Warehouse" />
       <div className="flex min-h-screen flex-col gap-10">
-        <TableStockItems />
+        <TableStockItems stockItems={stockItems} />
       </div>
     </DefaultLayout>
   );
--- src/components/Tables/TableStockItems.tsx
+++ src/components/Tables/TableStockItems.tsx
@@ -1,68 +1,12 @@
 import { StockItem } from "@/types/stockItem";
-import { InventoryAdjustmentEnum } from "@/types/enums/inventoryAdjustment.enum";
 import { TemperatureModeEnum } from "@/types/enums/temperatureMode.enum";
 import { exhaustiveCheck } from "@/shared/utils/exhaustiveCheck";
 
-const stockItemData: StockItem[] = [
-  {
-    id: "4c57581f-3051-4b51-b5bb-2fc79c8bb2a1",
-    itemName: "Aluminum",
-    description: "Rolls of aluminum",
-    isFlammable: false,
-    isFragile: false,
-    temperatureMode: TemperatureModeEnum.WITHOUT,
-    weightGrams: 100000,
-    inventoryAdjustment: InventoryAdjustmentEnum.NONE,
-    locationId: "215",
-    createdAt: new Date("2024-11-26T21:32:44.198Z"),
-    updatedAt: new Date("2024-11-26T21:32:44.198Z"),
-    removedAt: null,
-  },
-  {
-    id: "4c57581f-3051-4b51-b5bb-2fc79c1bb2a1",
-    itemName: "Cotton",
-    description: "Bags of cotton",
-    isFlammable: true,
-    isFragile: false,
-    temperatureMode: TemperatureModeEnum.WITHOUT,
-    weightGrams: 1000,
-    inventoryAdjustment: InventoryAdjustmentEnum.SURPLUS,
-    locationId: "215",
-    createdAt: new Date("2024-11-26T21:32:44.198Z"),
-    updatedAt: new Date("2024-11-26T21:32:44.198Z"),
-    removedAt: null,
-  },
-  {
-    id: "4c57581f-3051-4b51-b5bb-2fc79c7bb2a1",
-    itemName: "Meat",
-    description: "Bags with chicken meat",
-    isFlammable: false,
-    isFragile: false,
-    temperatureMode: TemperatureModeEnum.MINUS_15,
-    weightGrams: 10000,
-    inventoryAdjustment: InventoryAdjustmentEnum.NONE,
-    locationId: "215",
-    createdAt: new Date("2024-11-26T21:32:44.198Z"),
-    updatedAt: new Date("2024-11-26T21:32:44.198Z"),
-    removedAt: null,
-  },
-  {
-    id: "4c57581f-3051-4b51-b5bb-2fc79c7bb2a1",
-    itemName: "Electronics",
-    description: "PC components",
-    isFlammable: false,
-    isFragile: true,
-    temperatureMode: TemperatureModeEnum.WITHOUT,
-    weightGrams: 15000,
-    inventoryAdjustment: InventoryAdjustmentEnum.NONE,
-    locationId: "215",
-    createdAt: new Date("2024-11-26T21:32:44.198Z"),
-    updatedAt: new Date("2024-11-26T21:32:44.198Z"),
-    removedAt: null,
-  },
-];
+interface TableStockItemsProps {
+  stockItems: StockItem[];
+}
 
-const TableStockItems = () => {
+const TableStockItems = ({ stockItems }: TableStockItemsProps) => {
   return (
     <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="px-4 py-6 md:px-6 xl:px-7.5">
@@ -92,7 +36,7 @@ const TableStockItems = () => {
         </div>
       </div>
 
-      {stockItemData.map((item, key) => (
+      {stockItems.map((item, key) => (
         <div
           className="grid grid-cols-6 border-t border-stroke px-4 py-4.5 dark:border-strokedark sm:grid-cols-7 md:px-6 2xl:px-7.5"
           key={key}
