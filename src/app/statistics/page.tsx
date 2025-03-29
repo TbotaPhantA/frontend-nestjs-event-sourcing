0a26729a8d10d0c078600ca3519a5dbe92859a8b
TbotaPhantA <botakiller@mail.ru>
2025-03-29 14:42:21 +0000

    pie charts for isFlammable isFragile and temperatureMode tags were implemented

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -15,19 +15,45 @@ const StatisticsPage = () => {
   const [shippedItemsResponse, setShippedItemsResponse] =
     useState<FetchShippedItemsGraphDataResponse200>();
 
+  const [isFlammableCountData, setIsFlammableCountData] =
+    useState<FetchStatisticsProductsReceivedCountByTagsResponse200>();
+  const [isFragileCountData, setIsFragileCountData] =
+    useState<FetchStatisticsProductsReceivedCountByTagsResponse200>();
+  const [temperatureModeCountData, setTemperatureModeCountData] =
+    useState<FetchStatisticsProductsReceivedCountByTagsResponse200>();
+
   useEffect(() => {
     async function fetchData() {
-      const [receivingItemsGraphData, shippedItemsGraphData] =
-        await Promise.all([
-          fetchReceivedItemsGraphData(receivingCountWindow),
-          fetchShippedItemsGraphData(shippedCountWindow),
-        ]);
+      const [
+        receivingItemsGraphData,
+        shippedItemsGraphData,
+        isFlammableCountData,
+        isFragileCountData,
+        temperatureModeCountData,
+      ] = await Promise.all([
+        fetchReceivedItemsGraphData(receivingCountWindow),
+        fetchShippedItemsGraphData(shippedCountWindow),
+        fetchStatisticsProductsReceivedCountByTags(TagTypesEnum.isFlammable),
+        fetchStatisticsProductsReceivedCountByTags(TagTypesEnum.isFragile),
+        fetchStatisticsProductsReceivedCountByTags(
+          TagTypesEnum.temperatureMode,
+        ),
+      ]);
       setReceivingItemsResponse(receivingItemsGraphData);
       setShippedItemsResponse(shippedItemsGraphData);
+      setIsFlammableCountData(isFlammableCountData);
+      setIsFragileCountData(isFragileCountData);
+      setTemperatureModeCountData(temperatureModeCountData);
     }
 
     fetchData();
-  }, [receivingCountWindow, shippedCountWindow]);
+  }, [
+    receivingCountWindow,
+    shippedCountWindow,
+    isFlammableCountData,
+    isFragileCountData,
+    temperatureModeCountData,
+  ]);
 
   return (
     <DefaultLayout>
@@ -35,16 +61,12 @@ const StatisticsPage = () => {
 
       <div className="flex flex-col gap-10">
         <div className="flex flex-row justify-around">
+          <PieChart title={"Is flammable"} data={isFlammableCountData} />
+          <PieChart title={"Is fragile"} data={isFragileCountData} />
           <PieChart
-            data={[
-              { label: "a", value: 1 },
-              { label: "b", value: 2 },
-              { label: "c", value: 3 },
-            ]}
-            total={6}
+            title={"Temperature mode"}
+            data={temperatureModeCountData}
           />
-          {/*<PieChart />*/}
-          {/*<PieChart />*/}
         </div>
         <TurnoverChart
           name="Items received"
@@ -110,6 +132,42 @@ const fetchShippedItemsGraphData = async (
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ timeWindow: aggregationWindow }),
+      // Optionally disable caching for this request:
+      cache: "no-store",
+    },
+  );
+
+  // Check if the response is successful
+  if (!res.ok) {
+    throw new Error(`Failed to fetch data, status: ${res.status}`);
+  }
+
+  return await res.json();
+};
+
+export enum TagTypesEnum {
+  temperatureMode = "temperatureMode",
+  isFlammable = "isFlammable",
+  isFragile = "isFragile",
+}
+
+interface FetchStatisticsProductsReceivedCountByTagsResponse200 {
+  graphData: { label: string; value: number }[];
+  total: number;
+}
+
+const fetchStatisticsProductsReceivedCountByTags = async (
+  tag: TagTypesEnum,
+): Promise<FetchStatisticsProductsReceivedCountByTagsResponse200> => {
+  const res = await fetch(
+    "http://localhost:3001/storage/stock-month/statistics/products-received-count-by-tags",
+    {
+      method: "POST",
+      headers: {
+        accept: "*/*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ tag }),
       // Optionally disable caching for this request:
       cache: "no-store",
     },
